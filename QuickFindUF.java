975aee4560ac07eb3b240fb4b4ddf1cea1777e63
Abdul Wadood <awdon234@gmail.com>
2020-11-29 14:45:50 +0500

    QuickFindUF

--- QuickFindUF.java
+++ QuickFindUF.java
@@ -2,14 +2,7 @@ public class QuickFindUF {
     private int[] id;    // id[i] = component identifier of i
     private int count;   // number of connect components
 
-    /**
-     * Initializes an empty union-find data structure with
-     * {@code n} elements {@code 0} through {@code n-1}. 
-     * Initially, each elements is in its own set.
-     *
-     * @param  n the number of elements
-     * @throws IllegalArgumentException if {@code n < 0}
-     */
+
     public QuickFindUF(int n) {
         count = n;
         id = new int[n];
@@ -17,22 +10,12 @@ public class QuickFindUF {
             id[i] = i;
     }
 
-    /**
-     * Returns the number of sets.
-     *
-     * @return the number of sets (between {@code 1} and {@code n})
-     */
+  
     public int count() {
         return count;
     }
   
-    /**
-     * Returns the canonical element of the set containing element {@code p}.
-     *
-     * @param  p an element
-     * @return the canonical element of the set containing {@code p}
-     * @throws IllegalArgumentException unless {@code 0 <= p < n}
-     */
+   
     public int find(int p) {
         validate(p);
         return id[p];
@@ -46,17 +29,7 @@ public class QuickFindUF {
         }
     }
 
-    /**
-     * Returns true if the two elements are in the same set.
-     * 
-     * @param  p one element
-     * @param  q the other element
-     * @return {@code true} if {@code p} and {@code q} are in the same set;
-     *         {@code false} otherwise
-     * @throws IllegalArgumentException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
-     * @deprecated Replace with two calls to {@link #find(int)}.
-     */
+   
     @Deprecated
     public boolean connected(int p, int q) {
         validate(p);
@@ -64,15 +37,7 @@ public class QuickFindUF {
         return id[p] == id[q];
     }
   
-    /**
-     * Merges the set containing element {@code p} with the 
-     * the set containing element {@code q}.
-     *
-     * @param  p one element
-     * @param  q the other element
-     * @throws IllegalArgumentException unless
-     *         both {@code 0 <= p < n} and {@code 0 <= q < n}
-     */
+    
     public void union(int p, int q) {
         validate(p);
         validate(q);
@@ -87,15 +52,7 @@ public class QuickFindUF {
         count--;
     }
 
-    /**
-     * Reads an integer {@code n} and a sequence of pairs of integers
-     * (between {@code 0} and {@code n-1}) from standard input, where each integer
-     * in the pair represents some element;
-     * if the elements are in different sets, merge the two sets
-     * and print the pair to standard output.
-     * 
-     * @param args the command-line arguments
-     */
+    
     public static void main(String[] args) {
         int n = StdIn.readInt();
         QuickFindUF uf = new QuickFindUF(n);
